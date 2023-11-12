47bcd983fa4e44ff50fdc3137597449656075e75
Ezra Campbell <ezra_campbell_93@yahoo.com>
2023-11-12 09:33:04 -0500

    Start ignoring config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-//use this secretKey to sign the JSON Web Token (JWT) later
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
-}
